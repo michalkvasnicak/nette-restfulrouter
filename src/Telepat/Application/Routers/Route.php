a6ccbdc8ba7868550c83be3afaffe244528bc188
Michal Kvasnicak <michal.kvasnicak@gmail.com>
2012-12-20 15:54:37 +0100

    restful route implementation

--- src/Telepat/Application/Routers/Route.php
+++ src/Telepat/Application/Routers/Route.php
@@ -16,4 +16,71 @@ namespace Telepat\Application\Routers;
 class Route extends \Nette\Application\Routers\Route
 {
 
+
+    /**
+     * Possible methods (affects matching)
+     *
+     * @var array
+     */
+    protected $methods = [];
+
+
+    /**
+     * Creates Route
+     *
+     * @param array|string $method  array of methods or methods delimited by |
+     * @param array        $mask
+     * @param array        $metadata
+     * @param int          $flags
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function __construct($method, $mask, $metadata = array(), $flags = 0)
+    {
+        parent::__construct($mask, $metadata, $flags);
+
+        if (empty($method))
+        {
+            throw new \InvalidArgumentException('Method has to be set.');
+        }
+
+        if ( is_string($method) )
+        {
+            $methods = explode('|', trim($method, '|'));
+        }
+        elseif ( is_array($method) )
+        {
+            $methods = $method;
+        }
+        else
+        {
+            throw new \InvalidArgumentException('Method has to be string or array.');
+        }
+
+        // set methods to match
+        $this->methods = $methods;
+    }
+
+
+    /**
+     * Tries to match request
+     *
+     * @param \Nette\Http\IRequest $httpRequest
+     *
+     * @return \Nette\Application\Request|NULL
+     */
+    public function match(\Nette\Http\IRequest $httpRequest)
+    {
+        foreach ($this->methods as $method)
+        {
+            // method is not matched, return null
+            if ( $httpRequest->isMethod($method))
+            {
+                return parent::match($httpRequest);
+            }
+        }
+
+        return null;
+    }
+
 }
